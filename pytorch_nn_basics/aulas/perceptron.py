bb1843ee1f9eb86c225c6119e979d98c99da27f5
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-11-07 04:14:11 -0300

    Updates perceptrons linear equation exercise

--- pytorch_nn_basics/aulas/perceptron.py
+++ pytorch_nn_basics/aulas/perceptron.py
@@ -2,15 +2,20 @@ from utils import Aula
 import torch
 from typing import Union, List
 import numpy as np
+from matplotlib import pyplot as plt
+
 
 class AulaPerceptron(Aula):
     def __init__(self) -> None:
         super().__init__()
-        
+        self.res = ""
+
+        self.mostra_conteudo_sobre([self.equacao_da_reta(-1, 4, 0.4)])
+
     def mostra_conteudo_sobre(self, conteudo: Union[str, List[str]]) -> None:
-        return super().mostra_conteudo_sobre(conteudo) 
-    
-    def equacao_da_reta(self) -> str:
+        return super().mostra_conteudo_sobre(conteudo, cor="green")
+
+    def equacao_da_reta(self, a, b, c):
         """_summary_
             É aprendida na forma ax+by+c, mas para nos adequarmos às nomenclaturas de redes neurais,
             podemos reescrever essa equação como w1x1 + w2x2 + b. Ou seja, w1,w2 e b são parâmetros que definem um modelo linear,
@@ -18,4 +23,11 @@ class AulaPerceptron(Aula):
         Returns:
             str: _description_
         """
-        pass
+
+        # ax + by + c = 0
+        # y = (-a*x)/b
+        x = np.linspace(-2, 4, 50)
+        y = (-a * x) / b
+        plt.savefig("./assets/reta1.png")
+        self.res = str(x) + "\n" + str(y) + "\n" + "gráfico salvo em assets/reta1.png"
+        return f"Equação da reta|" + self.res
--- pytorch_nn_basics/main.py
+++ pytorch_nn_basics/main.py
@@ -1,7 +1,6 @@
-from aulas import (
-    intro_tensores 
-) 
+from aulas import intro_tensores, perceptron
 
 
-if __name__ == '__main__':
-    intro_tensores.AulaTensores([[1, 2, 3], [4, 5, 6]]) # tensor 2 de [3]
+if __name__ == "__main__":
+    intro_tensores.AulaTensores([[1, 2, 3], [4, 5, 6]])  # tensor 2 de [3]
+    perceptron.AulaPerceptron()
